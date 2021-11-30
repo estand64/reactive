ef0df80f795c2c3c6fa30d878a2ba623acc80a02
estand64 <estand64@gmail.com>
2021-11-30 08:05:07 -0700

    updates

--- reactive/src/main/java/com/example/reactive/services/BlockingService.java
+++ reactive/src/main/java/com/example/reactive/services/BlockingService.java
@@ -1,10 +1,10 @@
 package com.example.reactive.services;
 
+import com.example.reactive.dto.ReturnedItem;
 import com.example.reactive.webclients.BlockingWebClient;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Service
 public class BlockingService {
@@ -15,4 +15,36 @@ public class BlockingService {
         this.webClient = webClient;
         executorService = Executors.newFixedThreadPool(6);
     }
+
+    public void firstLogicalThought(){
+        CallData callA = new CallData("Call to A1", webClient::callFive);
+        CallData callA2 = new CallData("Call to A2", webClient::callTwo);
+        var prereqCall = processCall(null, callA);
+        var returnedCall = processCall(prereqCall, callA2);
+        System.out.println("Value returned from " + callA2.name + ": " + returnedCall.get());
+
+
+        CallData callB = new CallData("Call to B1", webClient::callFour);
+        CallData callB2 = new CallData("Call to B2", webClient::callOne);
+        CallData callB3 = new CallData("Call to B3", webClient::callOne);
+        prereqCall = processCall(null, callA);
+        returnedCall = processCall(prereqCall, callA2);
+        returnedCall = processCall(prereqCall, callA);
+    }
+
+    private Future<ReturnedItem> processCall(Future<ReturnedItem> prereq, CallData call){
+        if(prereq != null){
+            System.out.println("Waiting for a prereq call before processing: " + call.name());
+            try {
+                var returnedValue = prereq.get();
+                System.out.println("Got the value: " + returnedValue.id() + " from the prereq call!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Making a call to " + call.name());
+        return executorService.submit(call.call());
+    }
+
+    private static record CallData(String name, Callable<ReturnedItem> call){}
 }
